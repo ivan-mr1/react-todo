cdb81c6f686474c5de0e514f0c2cc0b879125681
Ivan <ivanitit3535@gmail.com>
2026-02-14 22:35:02 +0200

    init: start project

---
+++ README.md
@@ -0,0 +1,15 @@
+# To Do List React + Vite
+
+## Description
+
+landing To Do App
+
+- Deploy: [link]()
+
+- Technologies: SCSS, React, Vite, Eslint, Stylelint, Prettier
+
+- Screenshot:
+  <img alt="image" src="https://github.com/user-attachments/assets/33ca16a2-574e-44ff-988b-a0f33db992f1" />
+
+- Google Page Speed:
+  <img alt="image" src="https://github.com/user-attachments/assets/c1a633e4-625b-42df-b6b2-5f60a47c3631" />
---
+++ eslint.config.js
@@ -0,0 +1,56 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+const ignores = [
+  '**/node_modules/**',
+  '**/dist/**',
+  '**/build/**',
+  '**/coverage/**',
+  '**/*.log',
+  '**/npm-debug.log*',
+  '**/.git/**',
+  '**/.idea/**',
+  '**/.vscode/**',
+  '**/.DS_Store',
+  'postcss.config.js',
+  'vite.config.js',
+];
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    ignores,
+    plugins: {
+      js,
+      prettier: prettierPlugin,
+    },
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-console': 'warn',
+      eqeqeq: 'warn',
+      curly: 'warn',
+      'no-else-return': 'warn',
+    },
+  },
+]);
