264502339426f8313b72c50470e97fa7dc3b3519
Ivan <ivanitit3535@gmail.com>
2026-02-16 12:04:37 +0200

    feat: add events

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -10,17 +10,43 @@ const Todo = () => {
     { id: 'task-2', title: 'task name 2', isDone: true },
     { id: 'task-3', title: 'task name 3', isDone: false },
   ];
+
+  const deleteAllTasks = () => {
+    console.log('delete all tasks');
+  };
+
+  const deleteTask = (taskId) => {
+    console.log(`delete task ${taskId}`);
+  };
+
+  const toggleTaskComplete = (taskId, isDone) => {
+    console.log(`Task ${taskId} status: ${isDone ? 'done' : 'not done'}`);
+  };
+
+  const filterTasks = (query) => {
+    console.log(`Search: ${query}`);
+  };
+
+  const addTask = () => {
+    console.log('task added');
+  };
+
   return (
     <div className="todo">
       <h1 className="todo__title">To Do React</h1>
       <DateTime />
-      <AddTaskForm />
-      <SearchTaskForm />
+      <AddTaskForm addTask={addTask} />
+      <SearchTaskForm onSearchInput={filterTasks} />
       <TodoInfo
         total={tasks.length}
         done={tasks.filter(({ isDone }) => isDone).length}
+        onDeleteAllbuttonClick={deleteAllTasks}
       />
-      <TodoList tasks={tasks} />
+      <TodoList
+        tasks={tasks}
+        onDeleteTaskButtonClick={deleteTask}
+        onTaskCompleteChange={toggleTaskComplete}
+      />
     </div>
   );
 };
