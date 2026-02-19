c397953a5ffd46b8dbda24c828e1dd4a75249d21
Ivan <ivanitit3535@gmail.com>
2026-02-19 15:17:58 +0200

    fix: fix DateTime

--- src/components/DateTime.jsx
+++ src/components/DateTime.jsx
@@ -1,13 +1,27 @@
-const DateTime = () => {
-  const dateNow = new Date().toLocaleString(undefined, {
-    year: 'numeric',
-    month: '2-digit',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-  });
+import { useEffect, useState } from 'react';
 
-  return <div className="date">{dateNow}</div>;
+const formatter = new Intl.DateTimeFormat(undefined, {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+});
+
+const DateTime = () => {
+  const [now, setNow] = useState(() => new Date());
+
+  useEffect(() => {
+    const tick = () => {
+      setNow(new Date());
+    };
+
+    const intervalId = setInterval(tick, 60_000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return <div className="date">{formatter.format(now)}</div>;
 };
 
 export default DateTime;
